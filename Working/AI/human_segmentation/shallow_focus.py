4f59946d54a8dda1c01a74f1624bb8b334f73e00
SunCreation <cake0103@nate.com>
2021-10-27 18:34:25 +0900

    Focusing

--- Working/AI/human_segmentation/shallow_focus.py
+++ Working/AI/human_segmentation/shallow_focus.py
@@ -41,7 +41,7 @@ image_list = os.listdir(image_path)
 def select_img(image_list):
     n=0
     while n==0 :
-        print("어떤 사진을 shallow focus할까요?\n")
+        print("어떤 사진을 선택할까요?\n")
         num=1
 
         for i in image_list:
@@ -61,6 +61,7 @@ def select_img(image_list):
 
 n=0
 while n==0:
+    print('Shallow focusing 할 사진을 골라봅시다.')
     num = select_img(image_list)
     image_name = image_list[num-1]
     img_orig = cv2.imread(image_path + image_name)
@@ -143,6 +144,15 @@ print('model loaded successfully!')
 
 img_resized, seg_map = MODEL.run(img_orig)
 print ('원본 이미지 크기:', img_orig.shape,'수정 후 이미지 크기:', img_resized.shape,'최대 수치:', seg_map.max())
+
+#%%
+plt.figure(figsize=(10,10))
+plt.imshow(seg_map)
+plt.show()
+#%%
+for i in seg_map:
+    print(i)
+
 # %%
 import numpy as np
 import pandas as pd
@@ -179,7 +189,7 @@ seg_map = np.where(seg_map==num,15,0)
 
 plt.imshow(seg_map)
 plt.title(label_name+' segmentation map')
-#%%
+#%% 
 img_show = img_resized.copy()
 # seg_map = np.where(seg_map == 12, 15, 0) # 예측 중 사람만 추출, 위에 선택한 것만
 img_mask = seg_map * (255/seg_map.max()) # 255 normalization
@@ -202,22 +212,111 @@ plt.imshow(img_mask, cmap=plt.cm.binary_r)
 ax.set_title('DeepLab Model Mask')
 
 plt.show()
+
 # %%
+# 인물모드 배경 focus out
 img_orig_blur = cv2.blur(img_orig, (20,20)) #(13,13)은 blurring  kernel size를 뜻합니다. 
 plt.imshow(cv2.cvtColor(img_orig_blur, cv2.COLOR_BGR2RGB))
 plt.show()
+#%%
+# 배경 합성시 배경 선택
+n=0
+while n==0:
+    print('배경사진을 골라보겠습니다.')
+    num = select_img(image_list)
+    image_name = image_list[num-1]
+    img_bg = cv2.imread(image_path + image_name)
+    img_show_bg = img_bg.copy()
+    img_rgb_bg = cv2.cvtColor(img_bg, cv2.COLOR_BGR2RGB)    
+    plt.imshow(img_rgb_bg)
+    only_imagename = os.path.splitext(image_name)[0]
+    plt.title(only_imagename)
+    plt.show()
+    end=input("이 사진으로 하시겠어요? (y/n)\n\n:")
+    if end == 'y' or end == 'yes' or end == 'Y':
+        n=1
+
+
+#%%
+# 배경과 자연스럽게 합성할 수 있도록 잘라서 이미지 크기를 조정한다.
+def get_croped_image(img_orig,img_mask_up,bg_img,loc='down'):
+    x,y,_ = bg_img.shape
+    a,b = img_mask_up.shape
+    if loc == 'down':
+        new_a = b*x//y
+        croped_img = img_orig[-new_a:,:,:]
+        croped_img_mask = img_mask_up[-new_a:,:]
+        croped_img = cv2.resize(croped_img, (y,x))
+        croped_img_mask = cv2.resize(croped_img_mask, (y,x))
+    return croped_img, croped_img_mask
+
+croped_img, croped_img_mask = get_croped_image(img_orig,img_mask_up,img_bg)
+
+#%%
+plt.subplot(121)
+plt.imshow(cv2.cvtColor(croped_img, cv2.COLOR_BGR2RGB))
+plt.subplot(122)
+plt.imshow(croped_img_mask)
+plt.show()
+
 # %%
+# 인물사진 용
 img_mask_color = cv2.cvtColor(img_mask_up, cv2.COLOR_GRAY2BGR)
 img_bg_mask = cv2.bitwise_not(img_mask_color)
 img_bg_blur = cv2.bitwise_and(img_orig_blur, img_bg_mask)
 plt.imshow(cv2.cvtColor(img_bg_blur, cv2.COLOR_BGR2RGB))
 plt.show()
+
+#%%
+# 배경 합성용
+# img_bg_mask = cv2.bitwise_not(img_mask_color)
+# img_bg_blur = cv2.bitwise_and(img_orig_blur, img_bg_mask)
+# plt.imshow(cv2.cvtColor(img_bg_blur, cv2.COLOR_BGR2RGB))
+# plt.show()
 # %%
+# 인물사진용
 img_concat = np.where(img_mask_color==255, img_orig, img_bg_blur)
 plt.imshow(cv2.cvtColor(img_concat, cv2.COLOR_BGR2RGB))
 plt.show()
+#%%
+# 고양이용
+
+img_concat = np.where(img_mask_color==255, img_orig*2, img_bg_blur)
+plt.imshow(cv2.cvtColor(img_concat, cv2.COLOR_BGR2RGB))
+plt.show()
 # %%
-cv2.imwrite('output/'+only_imagename+'.png',img_concat)
+# 배경합성용
+img_mask_color = cv2.cvtColor(croped_img_mask, cv2.COLOR_GRAY2BGR)
+# croped_img의 화질 및 색감 변경
+croped_img_blur = cv2.blur(croped_img, (10,10)) #(13,13)은 blurring  kernel size를 뜻합니다. 
+new_croped_img = croped_img_blur//4 * 3 
+# 빼기로 하니 음수값이 이상하게 그려지고, 1.5로 나누면 오류가 난다. 3을 곱하고 4로 나누면 이상하게 더 어둡다
+img_concat = np.where(img_mask_color==255, new_croped_img, img_bg)
+img_concat_blur = cv2.blur(img_concat, (10,10)) #(13,13)은 blurring  kernel size를 뜻합니다. 
+plt.imshow(cv2.cvtColor(img_concat_blur, cv2.COLOR_BGR2RGB))
+plt.show()
+
+#%%
+# 인물사진용
+plt.figure(figsize=(10,20))
+plt.subplot(121)
+plt.imshow(cv2.cvtColor(img_orig, cv2.COLOR_BGR2RGB))
+plt.title('original '+only_imagename)
+plt.subplot(122)
+plt.title('focusing '+only_imagename)
+plt.imshow(cv2.cvtColor(img_concat, cv2.COLOR_BGR2RGB))
+plt.show()
+#%%
+# 배경 합성
+plt.figure(figsize=(10,20))
+plt.subplot(121)
+plt.imshow(cv2.cvtColor(img_bg, cv2.COLOR_BGR2RGB))
+plt.subplot(122)
+plt.imshow(cv2.cvtColor(img_concat_blur, cv2.COLOR_BGR2RGB))
+plt.show()
+
+# %%
+cv2.imwrite('output/'+only_imagename+'nice.png',img_concat)
 
 # %%
 import os
