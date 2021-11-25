d37c4f2a11d4727812848188b72d365d47fd0822
SunCreation <cake0103@nate.com>
2021-11-26 03:30:47 +0900

    Super_Resolution

--- Working/AI/Super_Resolution/super.py
+++ Working/AI/Super_Resolution/super.py
@@ -53,3 +53,89 @@ for i, (image, left_top, title) in enumerate(zip(images*3, left_tops, titles*3))
     plt.imshow(crop(image, left_top))
     plt.title(title, fontsize=20)
 # %%
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
+from silence_tensorflow import silence_tensorflow
+silence_tensorflow()
+
+# 데이터를 불러옵니다.
+train, valid = tfds.load(
+    "div2k/bicubic_x4", 
+    split=["train","validation"],
+    as_supervised=True
+)
+
+# 시각화를 위해 한 개의 데이터만 선택합니다.
+for i, (lr, hr) in enumerate(valid):
+    if i == 6: break
+    
+# 저해상도 이미지를 고해상도 이미지 크기로 bicubic interpolation 합니다.  
+hr, lr = np.array(hr), np.array(lr)
+bicubic_hr = cv2.resize(
+    lr, 
+    dsize=(hr.shape[1], hr.shape[0]), # 고해상도 이미지 크기로 설정
+    interpolation=cv2.INTER_CUBIC # bicubic 설정
+)
+
+# 저해상도 및 고해상도 이미지를 시각화합니다.
+plt.figure(figsize=(20,10))
+plt.subplot(1,2,1); plt.imshow(bicubic_hr)
+plt.subplot(1,2,2); plt.imshow(hr)
+
+# %%
+# 이미지의 특정 부분을 잘라내는 함수를 정의합니다.
+def crop(image, left_top, x=200, y=200):
+    return image[left_top[0]:(left_top[0]+x), left_top[1]:(left_top[1]+y), :]
+
+# interpolation된 이미지와 고해상도 이미지의 동일한 부분을 각각 잘라냅니다.
+left_top = (400, 500)
+crop_bicubic_hr = crop(bicubic_hr, left_top)
+crop_hr = crop(hr, left_top)
+
+# 잘라낸 부분을 시각화 합니다.
+plt.figure(figsize=(15,25))
+plt.subplot(1,2,1); plt.imshow(crop_bicubic_hr); plt.title("Bicubic", fontsize=30)
+plt.subplot(1,2,2); plt.imshow(crop_hr); plt.title("HR", fontsize=30)
+# %%
+import tensorflow as tf
+
+def preprocessing(lr, hr):
+    # 이미지의 크기가 크므로 (96,96,3) 크기로 임의 영역을 잘라내어 사용합니다.
+    hr = tf.image.random_crop(hr, size=[96, 96, 3])
+    hr = tf.cast(hr, tf.float32) / 255.
+    
+    # 잘라낸 고해상도 이미지의 가로, 세로 픽셀 수를 1/4배로 줄였다가
+    # interpolation을 이용해 다시 원래 크기로 되돌립니다.
+    # 이렇게 만든 저해상도 이미지를 입력으로 사용합니다.
+    lr = tf.image.resize(hr, [96//4, 96//4], "bicubic")
+    lr = tf.image.resize(lr, [96, 96], "bicubic")
+    return lr, hr
+
+train = train.map(preprocessing).shuffle(buffer_size=10).batch(16)
+valid = valid.map(preprocessing).batch(16)
+print("✅")
+# %%
+from tensorflow.keras import layers, Sequential
+
+# 3개의 convolutional layer를 갖는 Sequential 모델을 구성합니다.
+srcnn = Sequential()
+# 9x9 크기의 필터를 128개 사용합니다.
+srcnn.add(layers.Conv2D(128, 9, padding="same", input_shape=(None, None, 3)))
+srcnn.add(layers.ReLU())
+# 5x5 크기의 필터를 64개 사용합니다.
+srcnn.add(layers.Conv2D(64, 5, padding="same"))
+srcnn.add(layers.ReLU())
+# 5x5 크기의 필터를 64개 사용합니다.
+srcnn.add(layers.Conv2D(3, 5, padding="same"))
+
+srcnn.summary()
+# %%
+srcnn.compile(
+    optimizer="adam", 
+    loss="mse"
+)
+
+srcnn.fit(train, validation_data=valid, epochs=1)
+# %%
